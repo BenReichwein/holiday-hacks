667dd1808feef5ad51c1a8f8560e3208a0757ad7
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2020-12-16 22:36:44 -0500

    data to screen in home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,38 +1,35 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Home.css'
+
 const Serialize = require('../../net/serialize');
 
 class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            title: [],
-            img: [],
+            food: []
         }
-        new Serialize().data()
-        .then(r=> {
-            r.forEach(doc => {
+        Serialize.data()
+            .then(r => {
                 this.setState(prevState => ({
-                    title: [...prevState.title, doc.title],
-                    img: [...prevState.img, doc.img],
+                    food: r,
                 }));
             })
-        })
     }
 
     render() {
         return (
-        <div>
-            {this.state.title.map((item, index) => {
-                        return (
-                            <div className={"recipe"} key={index}>
-                                <h1>{this.state.title[index]}</h1>
-                                {/* eslint-disable-next-line jsx-a11y/alt-text */}
-                                <img src={this.state.img[index]}/>
-                            </div>
-                        )
-                    })}
-        </div>
+            <div>
+                {this.state.food.map((item, index) => {
+                    return (
+                        <div className={"recipe"} key={index}>
+                            <h1>{this.state.food[index].title}</h1>
+                            {/* eslint-disable-next-line jsx-a11y/alt-text */}
+                            <img src={this.state.food[index].img}/>
+                        </div>
+                    )
+                })}
+            </div>
         )
     }
 }
--- src/net/serialize.js
+++ src/net/serialize.js
@@ -2,14 +2,14 @@ const request = require('../net/http_requests.js');
 const foodModel = require('../models/food_model');
 
 class Serialize {
-    async get() {
-        return await request.get('recipes/complexSearch/?query=pizza')
+    static async get() {
+        return request.get('recipes/complexSearch/?query=pizza');
     }
 
-    async data() {
+    static async data() {
         let foodData = [];
         let foodModels = [];
-        await this.get().then(r => foodData.push(r));
+        await this.get().then(r => foodData.push(r))
         foodData = JSON.parse(foodData[0]);
         foodData['results'].forEach(element => {
             foodModels.push(new foodModel({
@@ -22,7 +22,5 @@ class Serialize {
         return foodModels;
     }
 }
-new Serialize().data().then(r=>console.log(r))
-//run this file to see layout of data
 
 module.exports = Serialize
