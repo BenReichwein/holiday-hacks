2b1e9b05b06f257066799164353703aa89993842
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2020-12-16 22:56:43 -0500

    simple loading notification

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,31 +7,37 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            food: []
+            food: [],
+            loading: true,
         }
         Serialize.data()
             .then(r => {
-                this.setState(prevState => ({
+                this.setState({
                     food: r,
-                }));
+                    loading: false
+                });
             })
     }
+
 
     render() {
         return (
             <div>
-                {this.state.food.map((item, index) => {
-                    return (
-                        <div className={"recipe"} key={index}>
-                            <h1>{this.state.food[index].title}</h1>
-                            {/* eslint-disable-next-line jsx-a11y/alt-text */}
-                            <img src={this.state.food[index].img}/>
-                        </div>
-                    )
-                })}
+                {this.state.loading ?
+                    <p>Loading...</p>
+                    : this.state.food.map((item, index) => {
+                        return (
+                            <div className={"recipe"} key={index}>
+                                <h1>{this.state.food[index].title}</h1>
+                                {/* eslint-disable-next-line jsx-a11y/alt-text */}
+                                <img src={this.state.food[index].img}/>
+                            </div>
+                        )
+                    })}
             </div>
         )
     }
+
 }
 
 export default Home;
