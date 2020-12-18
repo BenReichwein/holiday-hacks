2a0ef7fdd670e0700302be3b3914beb87701cd2a
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2020-12-18 13:49:11 -0500

    endpoint fix and refactoring

--- src/models/instructions_model.js
+++
@@ -1,8 +0,0 @@
-class InstructionsModel{
-    constructor({dishTypes, ingredients}) {
-        this.dishTypes = dishTypes;
-        this.ingredients = ingredients;
-    }
-}
-
-module.exports = InstructionsModel
