57772b904838cf19251f133ced206a82faec15ad
Ben <ben.basque@icloud.com>
2020-12-18 23:06:50 -0500

    Instructions, Ingredients needed, and some style has been completed

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Serialize from "../../net/serialize";
+import './Instructions.css'
 
 const keys = require('../../api_keys');
 
@@ -10,7 +11,8 @@ class Instructions extends Component {
         super(props);
         this.state = {
             instructions: [],
-            loading: true
+            ingredients: [],
+            loading: true,
         }
         Serialize.instructionData(`https://api.spoonacular.com/recipes/${this.props.location.item.id}/analyzedInstructions?includeNutrition=false&apiKey=${keys.food()}`, this.props.location.item)
             .then(r => {
@@ -18,17 +20,37 @@ class Instructions extends Component {
             })
     }
 
-
     render() {
-
-        let {instructions, loading} = this.state
-
-        return (
-            <div>
-                {loading ? <p>Loading...</p> : <p>{instructions.steps[0]['step']}</p>}
-
-            </div>
-        )
+        let {instructions, ingredients, loading} = this.state
+        if (loading === false) {
+            for (let i = 0; i < instructions.steps.length; i++) {
+                for (let x = 0; x < instructions.steps[i].ingredients.length; x++) {
+                    this.state.ingredients.push(instructions.steps[i].ingredients[x].name)
+                }
+            }
+            return (
+                <div className={'instructions'}>
+                    <p className={'instructions-needed'}><span>Ingredients Needed: </span>{ingredients.join(" ")}</p>
+                    <hr/>
+                    {instructions.steps.map((item, index) => {
+                                return (
+                                    <div key={index}>
+                                        <h5 className={'instructions-steps'}>Step #{index+1}</h5>
+                                        <p className={'instructions-step'}> {instructions.steps[index]['step']}</p>
+                                    </div>
+                                )
+                            }
+                        )
+                    }
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <p>Loading...</p>
+                </div>
+            )
+        }
     }
 
 }
